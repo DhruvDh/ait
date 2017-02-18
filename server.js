6661577077f1de551dec2095cb27a97480ffeced
DhruvDh <dhruv.dhamani@gmail.com>
2017-02-18 22:09:56 +0530

    Database Setup

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var dataStore = require('nedb');
+
+var db = new dataStore({filename: path.join(__dirname, 'db','db')});
+db.loadDatabase();
+
+db.insert({name: "Dhruv", class: "TE IT A"});
+db.insert({name: "Parth", class: "Third"});
 
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.get('/',function(req, res){
-    res.send('Yo whats up man');
-});
-
-app.get('/index.html',function(req, res){
     res.sendFile(path.join(__dirname, 'index.html'));
+    db.find({},function(err, docs){
+        console.log(docs);
+    });
 });
 
 var server = app.listen(process.env.PORT || '3000',function(){
