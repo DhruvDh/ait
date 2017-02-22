4f871229f6e8d5610650fc1c75d2ed826bd4bea9
DhruvDh <dhruv.dhamani@gmail.com>
2017-02-22 07:10:29 +0530

    Classifier Init

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 var express = require('express'),
+    bodyParser = require('body-parser')
     app = express();
 var path = require('path');
 var dataStore = require('nedb');
 var natural = require('natural'),
     intentCalssifier = new natural.BayesClassifier(),
-    subjectClassifier = new natural.BayesClassifier(),
+    subjectClassifier = new natural.BayesClassifier();
 
 
 var db = new dataStore({filename: path.join(__dirname, 'db','db')});
 db.loadDatabase();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
 
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
@@ -16,7 +23,14 @@ app.get('/',function(req, res){
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/training',function(req, res){
+    res.sendFile(path.join(__dirname, 'training.html'));
+});
+app.post('/training',function(req, res){
+    console.log(req.body.query);
+});
+
 var server = app.listen(process.env.PORT || '3000',function(){
-    classifier.addDocument('write se experiment 2', 'Write');
+    //classifier.addDocument('write se experiment 2', 'Write');
     console.log('The servers up yo.');
 });
